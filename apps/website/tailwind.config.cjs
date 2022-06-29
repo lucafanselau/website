12c5044ab7cfbadd31b51a6eb77fd469bddb08a4
Luca Fanselau <luca.fanselau@outlook.com>
2022-06-30 00:11:16 +0200

    Feels kinda good, i like the config so far...

---
+++ apps/website/tailwind.config.cjs
@@ -0,0 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			darker: "#24272F",
+			// text in light
+			dark: "#3E4251",
+			grey: "#555B6E",
+			green: "#89B0AE",
+			"light-green": "#BEE3DB",
+			"kinda-white": "#FAF9F9",
+			yellow: "#FFD6BA",
+		},
+		spacing: {
+			'1': '8px',
+			'2': '12px',
+			'3': '16px',
+			'4': '24px',
+			'5': '32px',
+			'6': '48px',
+			'7': '56px',
+			'8': '64px'
+		},
+		extend: {
+
+			fontFamily: {
+				"sans": ["Roboto Flex"], //, ...defaultTheme.fontFamily.sans]
+			}
+		},
+	},
+	plugins: [],
+}
