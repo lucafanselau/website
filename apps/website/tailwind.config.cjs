253bbc7265f4ac27b08701eb8830f2901d5eb735
Luca Fanselau <luca.fanselau@outlook.com>
2022-07-05 14:44:57 +0200

    Create a working framework for the blog

--- apps/website/tailwind.config.cjs
+++ apps/website/tailwind.config.cjs
@@ -5,16 +5,6 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		colors: {
-			darker: "#24272F",
-			// text in light
-			dark: "#3E4251",
-			grey: "#555B6E",
-			green: "#89B0AE",
-			"light-green": "#BEE3DB",
-			"kinda-white": "#FAF9F9",
-			yellow: "#FFD6BA",
-		},
 		spacing: {
 			'1': '8px',
 			'2': '12px',
@@ -26,7 +16,19 @@ module.exports = {
 			'8': '64px'
 		},
 		extend: {
-
+			boxShadow: {
+				"solid": "-8px 8px 0px rgba(43, 47, 59, 0.25)"
+			},
+			colors: {
+				darker: "#24272F",
+				// text in light
+				dark: "#3E4251",
+				grey: "#555B6E",
+				green: "#89B0AE",
+				"light-green": "#BEE3DB",
+				"kinda-white": "#FAF9F9",
+				yellow: "#FFD6BA",
+			},
 			fontFamily: {
 				"sans": ["Roboto Flex"], //, ...defaultTheme.fontFamily.sans]
 			}
