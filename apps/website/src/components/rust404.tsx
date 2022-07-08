8a7bf49e93412a1daedc33e5aef93148353bf007
Luca Fanselau <luca.fanselau@outlook.com>
2022-07-08 14:06:39 +0200

    Fix pointer lock issue

--- apps/website/src/components/rust404.tsx
+++ apps/website/src/components/rust404.tsx
@@ -4,6 +4,7 @@ import { DocumentEventListener, DocumentEventProps } from "@solid-primitives/eve
 
 export const Rust404: Component = () => {
     let canvas: HTMLCanvasElement;
+    let p: HTMLParagraphElement;
     let game: Game;
     let startup: number;
     let running = false;
@@ -15,39 +16,48 @@ export const Rust404: Component = () => {
         if (running) requestAnimationFrame(() => renderLoop(now));
     };
     const onClick: JSX.DOMAttributes<HTMLCanvasElement>["onClick"] = async event => {
+        const now = window.performance.now();
         if (game === undefined) {
             // initialize the whole shit
             await init();
             game = await Game.new();
             // initialize the
-            startup = window.performance.now();
+            startup = now;
         }
 
+        // NOTE: there is an issue with pointer lock in chrome (or others (not tested))
+        // https://discourse.threejs.org/t/how-to-avoid-pointerlockcontrols-error/33017
         if (!running) {
-            running = true;
             canvas.requestPointerLock();
-            // kickstart the render loop
-            renderLoop(startup);
         }
     };
 
     const lockChange: DocumentEventProps["onPointerlockchange"] = e => {
         if (document.pointerLockElement === canvas) {
             // that should be the action that we dispatched ourselves
+            running = true;
+            p.style.visibility = "hidden";
+            // kickstart the render loop
+            renderLoop(startup);
         } else {
             running = false;
+            p.style.visibility = "inherit";
+            p.textContent = "Click to continue the game";
         }
     };
 
     return (
         <div
             class={
-                "border-[3px] border-darker dark:border-kinda-white " +
+                "border-[3px] border-darker dark:border-kinda-white relative " +
                 "shadow-solid shadow-darker/25 dark:shadow-kinda-white/25 " +
                 "inline-block rounded-2xl dark:from-yellow dark:to-dark from-green to-yellow bg-gradient-to-l overflow-hidden"
             }
         >
             <canvas ref={canvas} id={"canvas"} width={600} height={400} onClick={onClick} />
+            <p ref={p} class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                Click to start the game
+            </p>
             <DocumentEventListener onPointerlockchange={lockChange} />
         </div>
     );
