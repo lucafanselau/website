579e9cf9573d1eb04349e3266722816cdd1b9fce
Luca Fanselau <luca.fanselau@outlook.com>
2022-07-09 12:11:31 +0200

    Commit wasm build (because no idea how vercel could build that)

---
+++ packages/rust-404/pkg/rust_404_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_game_free(a: number): void;
+export function game_new(): number;
+export function game_update(a: number, b: number, c: number): number;
+export function game_render(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbc4440f805ad3a11(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h08bca03dba1c3933(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h404bcf6503f407b6(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hae323a943c51c85e(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h8d39deb64f6d85f4(a: number, b: number, c: number, d: number): void;
