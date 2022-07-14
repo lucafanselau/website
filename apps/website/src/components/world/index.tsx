4ac1d3025a07978660b1d29cd99a7ff266157bdc
Luca Fanselau <luca.fanselau@outlook.com>
2022-07-14 18:55:00 +0200

    Fallback and stuff

--- apps/website/src/components/world/index.tsx
+++ apps/website/src/components/world/index.tsx
@@ -1,42 +1,14 @@
 /** @jsxImportSource react */
-
-import React, { FC, useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-
-function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef();
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-    // Return view, these are regular three.js elements expressed in JSX
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? 1.5 : 1}
-            onClick={event => setActive(!active)}
-            onPointerOver={event => setHover(true)}
-            onPointerOut={event => setHover(false)}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-        </mesh>
-    );
-}
+import { FC, lazy, Suspense } from "react";
 
 export const World: FC = () => {
+    const WorldImpl = lazy(() => import("./world"));
     return (
         <>
             <p>Hello from react</p>
-            <Canvas>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
-            </Canvas>
+            <Suspense fallback={<p>loading</p>}>
+                <WorldImpl />
+            </Suspense>
         </>
     );
 };
