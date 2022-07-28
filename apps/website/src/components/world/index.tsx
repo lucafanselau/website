fca74fdbd1c50d0a6b2e46ba60474a8c91e08689
Luca Fanselau <luca.fanselau@outlook.com>
2022-07-28 23:08:19 +0200

    EOD commit

--- apps/website/src/components/world/index.tsx
+++ apps/website/src/components/world/index.tsx
@@ -4,14 +4,10 @@ const WorldImpl = lazy(() => import("./world"));
 
 export const World: FC<{ children: ReactNode }> = ({ children }) => {
     return (
-        /* <div className="absolute left-0 top-0 w-full h-full -z-10">
-         *     <div className="sticky left-0 top-0 w-full aspect-square box-content"> */
         <div className="w-full h-full">
             <Suspense fallback={<p>loading</p>}>
                 <WorldImpl>{children}</WorldImpl>
             </Suspense>
         </div>
-        /* </div>
-    </div> */
     );
 };
--- apps/website/src/components/world/world.tsx
+++ apps/website/src/components/world/world.tsx
@@ -1,43 +1,39 @@
 /** @jsxImportSource react */
 
-import {
-    Backdrop,
-    ContactShadows,
-    Edges,
-    Environment,
-    Float,
-    Lightformer,
-    Scroll,
-    ScrollControls,
-    Stats,
-    useScroll,
-} from "@react-three/drei";
-import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
-import { Color, Depth, LayerMaterial, Noise } from "lamina";
+import { Environment, Html, Scroll, ScrollControls, Stats, useScroll } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
 import { FC, ReactNode, useMemo, useRef, useState } from "react";
-import { BackSide, Group, Mesh } from "three";
-import { PlanetGeometry } from "./planet";
+import { createPortal } from "react-dom";
+import { Group, Mesh } from "three";
 import { PlanetPolyhedronGeometry } from "./ico-planet";
+
+const mode = import.meta.env.MODE;
 /* extend({ PlanetGeometry }); */
 
-const Planet: FC = props => {
-    const options = useControls({
-        amplitude: {
-            value: 0.1,
-            step: 0.01,
-        },
-        frequency: 0.55,
-        octaves: {
-            value: 7,
-            step: 1,
-        },
-        persistence: {
-            value: 0.64,
-            step: 0.01,
-        },
-    });
+const options = {
+    amplitude: 0.1,
+    frequency: 0.55,
+    octaves: 7,
+    persistence: 0.64,
+};
+/* const options = useControls({
+ *     amplitude: {
+ *         value: 0.1,
+ *         step: 0.01,
+ *     },
+ *     frequency: 0.55,
+ *     octaves: {
+ *         value: 7,
+ *         step: 1,
+ *     },
+ *     persistence: {
+ *         value: 0.64,
+ *         step: 0.01,
+ *     },
+ * }); */
 
+const Planet: FC = props => {
     const geometry = useMemo(() => new PlanetPolyhedronGeometry(2, 32, options), [options]);
     /* const geometry = useMemo(() => new PlanetGeometry(2, options), [options]); */
 
@@ -46,8 +42,8 @@ const Planet: FC = props => {
     const parent = useRef<Group>(null);
     const ocean = useRef<Mesh>(null);
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
+    /* const [hovered, setHover] = useState(false);
+     * const [active, setActive] = useState(false); */
     const scroll = useScroll();
     const camera = useThree(s => s.camera);
     // Subscribe this component to the render-loop, rotate the mesh every frame
@@ -68,63 +64,38 @@ const Planet: FC = props => {
         <group ref={parent} position={[0, 0, 0]}>
             <mesh ref={ocean} position={[0, 0, 0]} receiveShadow>
                 <icosahedronGeometry args={[2, 10]} />
-                <meshStandardMaterial
-                    color={"#0496FF"}
-                    transparent
-                    opacity={0.7}
-                    flatShading={true}
-                    roughness={0.1}
-                    metalness={0.5}
-                    envMapIntensity={2}
-                />
+                <meshStandardMaterial color={"#0496FF"} transparent opacity={0.7} flatShading={true} roughness={0.1} metalness={0.5} />
             </mesh>
-            <mesh
-                {...props}
-                ref={mesh}
-                position={[0, 0, 0]}
-                onClick={event => setActive(!active)}
-                onPointerOver={event => setHover(true)}
-                onPointerOut={event => setHover(false)}
-                geometry={geometry}
-                receiveShadow
-            >
-                <meshStandardMaterial color={"#89B0AE"} flatShading={true} roughness={0.65} metalness={0.5} envMapIntensity={2} />
+            <mesh {...props} ref={mesh} position={[0, 0, 0]} geometry={geometry} receiveShadow>
+                <meshStandardMaterial color={"#89B0AE"} flatShading={true} roughness={0.65} metalness={0.5} />
             </mesh>
         </group>
     );
 };
 
-const Html: FC = () => {
+const Content: FC = () => {
     const size = useThree(s => s.size);
+    const domElement = useThree(s => s.gl.domElement);
 
-    return (
-        <Scroll html>
-            {/* <h1>html in here (optional)</h1>
-            <h1 style={{ position: "absolute", top: size.height }}>second page</h1>
-            <h1 style={{ position: "absolute", top: 2 * size.height }}>third page</h1> */}
-        </Scroll>
+    return createPortal(
+        <div className="absolute top-0 left-0 w-full h-full">
+            <h1 className="">Start scrolling...</h1>
+        </div>,
+        domElement
     );
 };
-function Striplight(props) {
-    return (
-        <mesh {...props}>
-            <boxGeometry />
-            <meshBasicMaterial color="white" />
-        </mesh>
-    );
-}
 const World: FC<{ children?: ReactNode }> = ({ children }) => {
     return (
         <Canvas>
             <ScrollControls pages={3}>
                 <Planet />
-                <Html />
+                <Content />
                 <spotLight position={[5, 15, 7]} angle={0.3} penumbra={1} castShadow intensity={0.5} shadow-bias={-0.0001} />
                 <directionalLight position={[-5, 5, 7]} castShadow intensity={0.7} shadow-bias={-0.0001} />
                 <ambientLight intensity={0.2} />
                 {/* <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={10} blur={3} opacity={1} far={10} /> */}
-                <Environment preset={"night"} />
-                <Stats />
+                <Environment preset={"sunset"} />
+                {mode === "development" && <Stats />}
             </ScrollControls>
         </Canvas>
     );
